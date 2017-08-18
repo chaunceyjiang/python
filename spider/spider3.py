27c2ac05f63c27bacfdee0eb43467cc4b1c659a0
Chauncey <goodstuden@qq.com>
2017-08-18 11:46:40 +0800

    Update spider3.py

--- spider/spider3.py
+++ spider/spider3.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 driver=webdriver.Chrome(r"C:\Python\phantomjs\bin\chromedriver.exe")
 driver.get("http://home.51cto.com/index")
-driver.find_element_by_id("loginform-username").send_keys("goodstuden@qq.com")
-driver.find_element_by_id("loginform-password").send_keys("jiangyan520")
+driver.find_element_by_id("loginform-username").send_keys("xxxxxx")
+driver.find_element_by_id("loginform-password").send_keys("xxxxxx")
 driver.find_element_by_name("login-button").click()
 driver.get("http://down.51cto.com/credits")
 driver.find_element_by_id("jsGetCredit").click()
